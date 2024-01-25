f4131f4879bca9ec020927c32fd7f9d163a3f773
Space V <40030799+ahcenezdh@users.noreply.github.com>
2024-01-25 20:12:34 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -4,7 +4,6 @@ lua54        'yes'
 games        { 'rdr3', 'gta5' }
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
---[[ Resource Information ]]--
 name         'RichPresence'
 author       'iDev & Co'
 version      '1.0.0'
